5d026016ff631bf2762f4bbf0cf9c8a3191ec537
Aaron Peckham <apeckham@local>
2011-07-27 19:30:13 -0700

    log referer

--- config.ru
+++ config.ru
@@ -1,5 +1,6 @@
 content = File.read(File.dirname(__FILE__) + "/500.html")
 
 run proc { |request|
+  puts "referer: #{request["HTTP_REFERER"]}"
   [500, {"Content-Type" => "text/html"}, [content]]
 }
--- test.rb
+++ test.rb
@@ -6,6 +6,15 @@ require "rack/test"
 
 class MyTest < Test::Unit::TestCase
   include Rack::Test::Methods
+  
+  def setup
+    @out = StringIO.new
+    $stdout = @out
+  end
+  
+  def teardown
+    $stdout = STDOUT
+  end
 
   def test_root
     get "/"
@@ -17,6 +26,12 @@ class MyTest < Test::Unit::TestCase
     get "/anything"
     assert_equal 500, last_response.status
     assert_match /Something went wrong/, last_response.body
+  end
+  
+  def test_log_referer
+    get "/anything", {}, {"HTTP_REFERER" => "http://www.youtube.com/"}
+    @out.rewind
+    assert_match %r{http://www.youtube.com/}, @out.read
   end
   
   def app
